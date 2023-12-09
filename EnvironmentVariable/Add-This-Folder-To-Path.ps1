7dc3653de93d4946e004ab9ff186743b3abaadff
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2023-12-09 23:57:45 +0200

    Add Script to Add Folder To Path

---
+++ EnvironmentVariable/Add-This-Folder-To-Path.ps1
@@ -0,0 +1,6 @@
+$variable = $args[0];
+$path = [Environment]::GetEnvironmentVariable('path', [EnvironmentVariableTarget]::User);
+$path += ";$variable";
+[Environment]::SetEnvironmentVariable('Path', $path, [EnvironmentVariableTarget]::User);
+Write-Output "New Path Variable Is $path";
+Read-Host "PRESS ANY KEY TO EXIT."
