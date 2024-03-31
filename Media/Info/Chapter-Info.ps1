5612ea8685af412b2ff650eccf2e5b4f0392e9fc
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-03-31 10:30:23 +0200

    Enhance Chapter Based Sub

--- Media/Info/Chapter-Info.ps1
+++ Media/Info/Chapter-Info.ps1
@@ -20,9 +20,56 @@ function GetChapters {
         }
     }
 
+    $openeingChapter = $chapters | Where-Object { $_.Title -match "(?i)(^Op - )|(Opening)" }
+    $realChapters = $chapters | Where-Object {
+        $_ -ne $openeingChapter
+    } | Sort-Object -Property Start, End, Length
+    
+    for ($i = 0; $i -lt $realChapters.Count; $i++) {
+        $chapter = $realChapters[$i];
+        if ($i -eq $realChapters.Count - 1) {
+            $chapter.Diff = 0;
+            continue;
+        }
+        $nextChapter = $realChapters[$i + 1];
+        $chapter.Diff = $nextChapter.Start - $chapter.End;
+    }
+
     # Output the chapters array
-    return $chapters
+    return @($openeingChapter) + $realChapters
 }
 
-$chapters = GetChapters -path $file | Sort-Object -Property Start, End;
-Write-Host "DONE";
+foreach ($file in ($args | Where-Object { $_ -match ".*(.mkv)$" })) {
+    $chapters = GetChapters -path $file | Sort-Object -Property Start, End;
+    $openeingChapter = $chapters | Where-Object { $_.Title -match "(?i)(^Op - )|(Opening)" }
+    Write-Host $file -ForegroundColor Blue;
+    Write-Host "$($openeingChapter.Title): $($openeingChapter.Start) -> $($openeingChapter.End) With Length $($openeingChapter.Length)"
+    $chapters = $chapters | Where-Object {
+        $_ -ne $openeingChapter
+    }
+    $totalDiff = 0;
+    foreach ($chapter in $chapters) {
+        if ($chapter.Diff -eq 0) {
+            continue;
+        }
+
+        if ($totalDiff -gt 0) {
+            $totalDiff -= $chapter.Diff
+
+        }
+        else {
+            $totalDiff += $chapter.Diff
+        }
+    }
+    Write-Host "totalDiff: $totalDiff";
+    foreach ($chapter in $chapters) {
+        Write-Host "$($chapter.Title): $($chapter.Start) -> $($chapter.End) With Length $($chapter.Length), DIFF $($chapter.Diff)"
+    }
+}
+
+function ForceExit {
+    Read-Host "Force Close The Window";
+    ForceExit;
+}
+
+ForceExit
--- Media/Subtitles/Subtitle-Shifter/custom/Chapter-Based-Shifter.ps1
+++ Media/Subtitles/Subtitle-Shifter/custom/Chapter-Based-Shifter.ps1
@@ -1,10 +1,3 @@
-$ffmpegPath = "D:\Programs\Media\Tools\yt\ffmpeg.exe"
-$chapterNames = @("Intro");
-$handlers = @{
-    ".ass" = "D:\Education\Projects\MyProjects\Shell-Scripts\Media\Subtitles\Subtitle-Shifter/handlers/Ass-Subtitle-Shifter.ps1";
-    ".srt" = "D:\Education\Projects\MyProjects\Shell-Scripts\Media\Subtitles\Subtitle-Shifter/handlers/Srt-Subtitle-Shifter.ps1";
-};
-
 #region function
 
 function GetChapters {
@@ -32,15 +25,55 @@ function GetChapters {
 }
 
 #endregion
+
+$colors = @(
+    [System.ConsoleColor]::Black,
+    [System.ConsoleColor]::DarkBlue,
+    [System.ConsoleColor]::DarkGreen,
+    [System.ConsoleColor]::DarkCyan,
+    [System.ConsoleColor]::DarkMagenta,
+    [System.ConsoleColor]::DarkYellow,
+    [System.ConsoleColor]::Gray,
+    [System.ConsoleColor]::DarkGray,
+    [System.ConsoleColor]::Blue,
+    [System.ConsoleColor]::Green,
+    [System.ConsoleColor]::Cyan,
+    [System.ConsoleColor]::Magenta,
+    [System.ConsoleColor]::Yellow
+);
+
+$ffmpegPath = "D:\Programs\Media\Tools\yt\ffmpeg.exe"
+$chapterNames = @("Intro");
+$handlers = @{
+    ".ass" = "D:\Education\Projects\MyProjects\Shell-Scripts\Media\Subtitles\Subtitle-Shifter/handlers/Ass-Subtitle-Shifter.ps1";
+    ".srt" = "D:\Education\Projects\MyProjects\Shell-Scripts\Media\Subtitles\Subtitle-Shifter/handlers/Srt-Subtitle-Shifter.ps1";
+};
+
+$startFromChapterNames = @("Episode");
+$chapterNames = Read-Host "Please enter chapter names to start delaying from its start?";
+if ($chapterNames) {
+    $startFromChapterNames = $startFromChapterNames + ($chapterNames -split "," | ForEach-Object { return $_.Trim() })
+}
+
+$startAtTheEndOfChapterNames = @("Intro");
+$chapterNames = Read-Host "Please enter chapter names to start delaying from its end?";
+if ($chapterNames) {
+    $startAtTheEndOfChapterNames = $startAtTheEndOfChapterNames + ($chapterNames -split "," | ForEach-Object { return $_.Trim() })
+}
+
+
+$global:customDelay = [double](Read-Host "Custom Delay?");
+# $global:additionalDelayInMiliSeconds = [double](Read-Host "Please enter additional Delay In Seconds?") * 1000;
 $global:manualDelayInMiliSeconds = $null;
 function Handle {
     param ($videoFile, $subFile, $handler)
     $chapters = GetChapters -path $videoFile;
+    Write-Host ($chapters.Title) -Separator ", "
     $startFromSecond = 0;
     $openeingChapter = $chapters | Where-Object { $_.Title -match "(?i)(^Op - )|(Opening)" }
     if (!$openeingChapter) {
         Write-Host "Can't find Opening chapter for $videoFile" -ForegroundColor Red -BackgroundColor White;
-
+        return;
         if ($null -eq $global:manualDelayInMiliSeconds) {
             $global:manualDelayInMiliSeconds = [double](Read-Host "Please Enter Delay in seconds?");
             if (!$global:manualDelayInMiliSeconds) {
@@ -53,17 +86,29 @@ function Handle {
         $delayMilliseconds = $openeingChapter.Length * 1000 * -1;
     }
 
-    $episodeChapter = $chapters | Where-Object { $_.Title -match "Episode|(Part(-| )A)" }
+    $episodeChapter = $chapters | Where-Object { $startFromChapterNames -contains $_.Title }
     if ($episodeChapter) {
         $startFromSecond = $episodeChapter.Start;
     }
     else {
-        $targetChapter = $chapters | Where-Object { $_.Title -in $chapterNames }
-        if ($targetChapter) {
-            $startFromSecond = $targetChapter.End
+        Write-Host "Can't Find Chapters to Start From ts Start Search By $startFromChapterNames" -ForegroundColor Red;
+        $introChapter = $chapters | Where-Object { $startAtTheEndOfChapterNames -contains $_.Title }
+        if ($introChapter) {
+            $startFromSecond = $introChapter.End
+            if ($introChapter.End -lt $openeingChapter.End) {
+                # $delayMilliseconds += 1000;
+            }
+        }
+        else {
+            Write-Host "Can't Find Chapters to Start From Its End Search By $startAtTheEndOfChapterNames" -ForegroundColor Red;
         }
     }
-
+    
+    $delayMilliseconds += $global:additionalDelayInMiliSeconds;
+    
+    if ($global:customDelay) {
+        $delayMilliseconds = $global:customDelay;
+    }
     if ($startFromSecond) {
         $startFromSecond = "startFromSecond=$($startFromSecond)";
         & $handler  "file=$subFile" $startFromSecond "delayMilliseconds=$delayMilliseconds";
@@ -73,13 +118,17 @@ function Handle {
     }
 }
 
-foreach ($file in $args) {
+$files = $args | Where-Object { $_ -match ".*(ass|srt)$" }
+foreach ($file in $files) {
     $info = Get-Item -LiteralPath $file;
     $videInfo = "$($info.Directory)/$($info.Name -replace $info.Extension, ".mkv")"
+    $color = Get-Random $colors;
+    Write-Host "Start Handling File: $subFile" -ForegroundColor $color;
     Handle `
         -handler $handlers[$info.Extension] `
         -videoFile $videInfo `
         -subFile $file;
+    Write-Host "Start Handling File: $subFile" -ForegroundColor $color;
 }
 
 Read-Host "Press Any Key To Exit."
