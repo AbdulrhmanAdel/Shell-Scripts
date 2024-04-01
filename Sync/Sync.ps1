e7868a1770f59b29a9801a4d51a966591fdf1aae
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-04-01 21:29:20 +0200

    Add Game Saves To Sync

--- Sync/Sync.ps1
+++ Sync/Sync.ps1
@@ -5,6 +5,7 @@ function Sync {
     2- Programs
     3- Education Programs
     4- Education Projects
+    5- Game Saves
 ");
 
     $mirrorConfig = "D:\Programs\Tools\Free File Sync\Configs\Mirror.ffs_gui";
@@ -15,18 +16,27 @@ function Sync {
         1 {
             $command += " ""$updateConfig""";
             $command += " -DirPair ""D:\Watch"" ""E:\Watch"""; 
+            break;
         }
         2 {
             $command += " ""$mirrorConfig""";
             $command += " -DirPair ""D:\Programs"" ""E:\Programs""";  
+            break;
         }
         3 {
             $command += " ""$mirrorConfig""";
             $command += " -DirPair ""D:\Education\Programs"" ""E:\Education\Programs"""; 
+            break;
         }
         4 { 
             $command += " ""D:\Programs\Tools\Free File Sync\Configs\Education Projects.ffs_gui""";
             $command += " -DirPair ""D:\Education\Projects"" ""E:\Education\Projects"""; 
+            break;
+        }
+        5 { 
+            $command += " ""D:\Programs\Tools\Free File Sync\Configs\Education Projects.ffs_gui""";
+            $command += " -DirPair ""D:\Personal\Game Saves"" ""E:\Personal\Game Saves""";
+            break;
         }
         Default {
             exit
