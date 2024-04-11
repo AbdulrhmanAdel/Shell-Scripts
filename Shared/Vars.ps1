f4bf3f3a758e05af0f01597bb9f7f2cdcda9eb95
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-04-11 20:23:26 +0200

    Clean Up Scripts

---
+++ Shared/Vars.ps1
@@ -0,0 +1,6 @@
+$ytDlp = "D:\Programs\Media\Tools\yt\yt-dlp.exe";
+$mkvMerge = "D:\Programs\Media\Tools\mkvtoolnix\mkvmerge.exe";
+$mkvExtract = "D:\Programs\Media\Tools\mkvtoolnix\mkvextract.exe";
+$mediaInfo = "D:\Programs\Media\Tools\MediaInfo\MediaInfo.exe";
+$ffmpeg = "D:\Programs\Media\Tools\yt\ffmpeg.exe";
+$handbrake = "D:\Programs\Media\Tools\HandBrake\HandBrake CLI\HandBrakeCLI.exe";
