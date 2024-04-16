5e2eb499a6408d264917a7ddff1e8a808b0d0746
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-04-16 16:03:26 +0200

    Enhance Crop To Enter Time Span Instead Of Total Seconds

--- Media/Crop.ps1
+++ Media/Crop.ps1
@@ -2,8 +2,34 @@ $ffmpeg = "D:\Programs\Media\Tools\yt\ffmpeg.exe";
 $filePath = $args[0];
 $fileExtension = [System.IO.Path]::GetExtension($filePath);
 $outputPath = $filePath.Replace($fileExtension, ".cropped$fileExtension");
-$start = [int](Read-Host "Start?");
-$end = [int](Read-Host "End?");
+
+#region Functions
+Write-Host "Allowed Sperators => (Space or : or , or |)";
+function Prompt {
+    param (
+        [string]$key
+    )
+    
+    $timeInput = (Read-Host "$key") -split ":| |,|\|";
+    if (!$timeInput) {
+        return 0;
+    }
+
+    $timeSpan = [timespan]::FromSeconds($timeInput[$timeInput.Length - 1]);
+    if ($timeInput.Length -eq 3) {
+        $timeSpan += [timespan]::FromMinutes($timeInput[1]);
+        $timeSpan += [timespan]::FromHours($timeInput[0]);
+    }
+    elseif ($timeInput.Length -eq 2) {
+        $timeSpan += [timespan]::FromMinutes($timeInput[0]);
+    }
+
+    return $timeSpan.TotalSeconds;
+}
+#endregion
+
+$start = Prompt -key "Start?"
+$end = Prompt -key "End?"
 if ($end -eq 0) {
     exit;
 }
@@ -14,4 +40,4 @@ $commandArgs = @(
     "-i", """$filePath""",
     """$outputPath""");
 
-Start-Process -FilePath $ffmpeg -ArgumentList $commandArgs -NoNewWindow;
+Start-Process $ffmpeg -ArgumentList $commandArgs -NoNewWindow;
