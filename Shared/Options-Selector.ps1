abb3e9e2d1f37f9c89a791104fdf676f7d3cf791
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-04-21 10:55:51 +0200

    Enhance Option Selector Support Use Option Selector in Sync

--- Shared/Options-Selector.ps1
+++ Shared/Options-Selector.ps1
@@ -1,4 +1,5 @@
 Add-Type -AssemblyName System.Windows.Forms
+
 function ParseArgs {
     param ($list, [string]$key)
     $value = $list | Where-Object { $null -ne $_ -and $_.ToString().StartsWith("$key=") };
@@ -11,22 +12,35 @@ $options = $args[0];
 $form = New-Object System.Windows.Forms.Form
 $form.Text = (ParseArgs -list $args -key "title") ?? 'Select an Option';
 $form.StartPosition = 'CenterScreen'
+$form.Font = New-Object System.Drawing.Font("Segoe UI", 10)
+$form.Width = 500;
+$form.BackColor = [System.Drawing.Color]::FromArgb(240, 240, 240)
 
 $global:selectedOption = $null
 
 function ButtonClicked {
     param($senderObj, $e)
-    $global:selectedOption = $senderObj.Text
+    $global:selectedOption = $senderObj.Tag;
     $form.DialogResult = [System.Windows.Forms.DialogResult]::OK
 }
 
 $flowLayoutPanel = New-Object System.Windows.Forms.FlowLayoutPanel
 $flowLayoutPanel.Dock = [System.Windows.Forms.DockStyle]::Fill
+$flowLayoutPanel.AutoScroll = $true
+$flowLayoutPanel.Padding = New-Object System.Windows.Forms.Padding(10)  # Add padding
 $form.Controls.Add($flowLayoutPanel)
 
 function CreateButton($text) {
     $button = New-Object System.Windows.Forms.Button
     $button.Text = $text
+    $button.Tag = $text
+    $button.AutoSize = $true
+    $button.AutoSizeMode = [System.Windows.Forms.AutoSizeMode]::GrowAndShrink
+    $button.BackColor = [System.Drawing.Color]::FromArgb(255, 255, 255)
+    $button.ForeColor = [System.Drawing.Color]::FromArgb(0, 0, 0)
+    $button.FlatStyle = [System.Windows.Forms.FlatStyle]::Flat
+    $button.Font = New-Object System.Drawing.Font("Segoe UI", 9)
+    $button.Padding = New-Object System.Windows.Forms.Padding(5)  # Add button padding
     $button.Add_Click({ ButtonClicked $args[0] $args[1] })
     $flowLayoutPanel.Controls.Add($button)
 }
--- Tools/Sync Files.ps1
+++ Tools/Sync Files.ps1
@@ -1,60 +1,82 @@
 $syncProgram = "D:\Programs\Tools\Free File Sync\FreeFileSync.exe";
 
 Write-Host "GETTING DRIVE LETTER..." -ForegroundColor Green
-$disk = Get-Disk -FriendlyName "WD My Passport 2626"
-$part = Get-Partition -DiskNumber $disk.DiskNumber;
+$disk = Get-Disk -FriendlyName "WD My Passport 2626" -ErrorAction Ignore
+$part = Get-Partition -DiskNumber $disk.DiskNumber -ErrorAction Ignore;
 $driveLetter = $part.DriveLetter;
+if (!$driveLetter) {
+    Write-Host "CAN'T RETRIEVED DRIVE LETTER" -ForegroundColor Red
+    timeout 5;
+    EXIT;
+}
 Write-Host "DRIVE LETTER RETRIEVED: $driveLetter" -ForegroundColor Green
+enum SyncTarget {
+    Watch    
+    Programs
+    ProgrammingPrograms
+    ProgrammingProjects
+    GameSaves
+    ProgramsData
+    Fortnite
+    Quarn
+    Cancel
+}
+
+$mirrorConfig = "D:\Programs\Tools\Free File Sync\Configs\Mirror.ffs_gui";
+$updateConfig = "D:\Programs\Tools\Free File Sync\Configs\Update.ffs_gui"
 function Sync {
-    $type = [int](Read-Host "
-    1- Watch    
-    2- Programs
-    3- Programming Programs
-    4- Programming Projects
-    5- Game Saves
-    6- Programs Data
-    7- Fortnite
-");
-
-    $mirrorConfig = "D:\Programs\Tools\Free File Sync\Configs\Mirror.ffs_gui";
-    $updateConfig = "D:\Programs\Tools\Free File Sync\Configs\Update.ffs_gui"
+    $type = & Options-Selector.ps1 ([SyncTarget].GetEnumNames());
+    
+    if ($type) {
+        $type = [SyncTarget]::$($type);
+    }
+    
     $command = ""
-
+    [SyncTarget]::Watch
     switch ($type) {
-        1 {
+        ([SyncTarget]::Watch) {
             $command += " ""$updateConfig""";
             $command += " -DirPair ""D:\Watch"" ""$($driveLetter):\Watch"""; 
             break;
         }
-        2 {
+    
+        ([SyncTarget]::Programs) {
             $command += " ""$mirrorConfig""";
             $command += " -DirPair ""D:\Programs"" ""$($driveLetter):\Programs""";  
             break;
         }
-        3 {
+        ([SyncTarget]::ProgrammingPrograms) {
             $command += " ""D:\Programs\Tools\Free File Sync\Configs\Programming Programs.ffs_gui""";
             $command += " -DirPair ""D:\Programming\Programs"" ""$($driveLetter):\Programming\Programs"""; 
             break;
         }
-        4 { 
+        ([SyncTarget]::ProgrammingProjects) { 
             $command += " ""D:\Programs\Tools\Free File Sync\Configs\Programming Projects.ffs_gui""";
             $command += " -DirPair ""D:\Programming\Projects"" ""$($driveLetter):\Programming\Projects"""; 
             break;
         }
-        5 { 
+        ([SyncTarget]::GameSaves) { 
             $command += " ""D:\Programs\Tools\Free File Sync\Configs\Programming Projects.ffs_gui""";
             $command += " -DirPair ""D:\Personal\Game Saves"" ""$($driveLetter):\Personal\Game Saves""";
             break;
         }
-        6 {
+        ([SyncTarget]::ProgramsData) {
             $command += " ""$mirrorConfig""";
             $command += " -DirPair ""D:\Programming\Programs\1- Programs Data"" ""$($driveLetter):\Programming\Programs\1- Programs Data""";
             break;
         }
-        7 {
+        ([SyncTarget]::Fortnite) {
             $command += " ""$mirrorConfig""";
             $command += " -DirPair ""D:\Games\Fortnite"" ""$($driveLetter):\Games\Fortnite""";
             break;
+        }
+        ([SyncTarget]::Quarn) {
+            $command += " ""$mirrorConfig""";
+            $command += " -DirPair ""D:\Personal\Media\القرأن الكريم"" ""$($driveLetter):\Personal\Media\القرأن الكريم""";
+            break;
+        }
+        ([SyncTarget]::Cancel) {
+            Exit;
         }
         Default {
             Sync
