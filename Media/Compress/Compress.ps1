ff19f4024c556bf71afe5b58ecbdb4684308b795
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-04-25 14:42:55 +0200

    Enhance Safe-Delete

--- Media/Compress/Compress.ps1
+++ Media/Compress/Compress.ps1
@@ -1,5 +1,24 @@
 $videos = @();
 $images = @();
+
+function CopyTransformedToOriginalFolder {
+    param (
+        $files
+    )
+
+    $files | ForEach-Object {
+        $path = $_;
+        $info = Get-Item -LiteralPath $path;
+        $originalFolder = "$($info.Directory.FullName)\Original";
+        if (Test-Path -LiteralPath $originalFolder) {
+            return;
+        }
+    
+        New-Item -Path $originalFolder -ItemType Directory;
+        Move-Item -Path $path -Destination $originalFolder
+    }
+}
+
 $args | ForEach-Object {
     if (!(Test-Path -LiteralPath $_)) {
         return $false; 
@@ -19,11 +38,14 @@ if ($source -match "All|Images" -and $images.Count) {
     $command = (@("""$modulesPath\Image-Compressor.ps1""") + $images) -join " ";
     Write-Host "ENCODING IMAGES" -ForegroundColor Magenta;
     Start-Process  pwsh.exe -ArgumentList $command -Wait -NoNewWindow;
+    CopyTransformedToOriginalFolder -files $images;
+
     Write-Host "FINISH ENCODING IMAGES" -ForegroundColor Magenta;
 }
 if ($source -match "All|Videos" -and $videos.Count) {
     $command = (@("""$modulesPath\Video-Compressor.ps1""") + $videos) -join " ";
     Write-Host "ENCODING Videos" -ForegroundColor Magenta;
     Start-Process  pwsh.exe -ArgumentList $command -Wait -NoNewWindow;
+    CopyTransformedToOriginalFolder -files $videos;
     Write-Host "FINISH ENCODING VIDEOS" -ForegroundColor Blue;
 }
--- Media/Compress/Modules/Video-Compressor.ps1
+++ Media/Compress/Modules/Video-Compressor.ps1
@@ -1,37 +1,8 @@
-$selector = "Options-Selector.ps1";
-$width, $height = (((& $selector @("480x640", "720x1280", "1080x1920")) -split "x") | ForEach-Object { return [int]$_ }) ?? @(720, 1280);
-$encoder = (& $selector @("x264", "x265", "x265_10bit")) ?? "x264";
-$encoderPreset = (& $selector @( 
-        "ultrafast",
-        "superfast",
-        "veryfast",
-        "faster",
-        "fast",
-        "medium"
-    ) title="Select Encoder Preset") ?? "veryfast";
-$allowedExtensions = "mkv$|mp4$";
-$files = @(
-    $args | Where-Object {
-        if (!(Test-Path -LiteralPath $_)) {
-            return $false;
-            
-        }
-        return $_ -match $allowedExtensions;
-    });
-
-$sharedArgs = @(
-    "--encoder", $encoder,
-    "-q", 23,
-    "-T",
-    "--optimize",
-    "--width", $height,
-    "--height", $width,
-    "-r", 30, "--pfr",
-    "--verbose=0",
-    "--encoder-preset", $encoderPreset
-);
-
-$files | ForEach-Object {
+function Compress {
+    param (
+        $file
+    )
+    
     $file = $_;
     Write-Host "Compressing $file" -ForegroundColor Green;
     $fileInfo = Get-Item -LiteralPath $file;
@@ -45,3 +16,48 @@ $files | ForEach-Object {
     Start-Process handbrake -ArgumentList $arguments -NoNewWindow -PassThru -Wait;
     Write-Host "Compressing $file" -ForegroundColor Green;
 }
+
+$selector = "Options-Selector.ps1";
+$width, $height = (((& $selector @("480x640", "720x1280", "1080x1920") "title=Select Video Resoluation") -split "x") | ForEach-Object { return [int]$_ }) ?? @(720, 1280);
+$encoder = (& $selector @("x264", "x265", "x265_10bit") "title=Select Encoder Target") ?? "x264";
+$encoderPreset = (& $selector @( 
+        "ultrafast",
+        "superfast",
+        "veryfast",
+        "faster",
+        "fast",
+        "medium"
+    ) "title=Select Encoding Speed Preset") ?? "veryfast";
+
+$sharedArgs = @(
+    "--encoder", $encoder,
+    "-q", 23,
+    "-T",
+    "--optimize",
+    "--width", $height,
+    "--height", $width,
+    "-r", 30, "--pfr",
+    "--verbose=0",
+    "--encoder-preset", $encoderPreset
+);
+
+$allowedExtensions = "mkv$|mp4$";
+$args | ForEach-Object {
+    $info = Get-Item -LiteralPath $_ -ErrorAction Ignore;
+    if (!$info) {
+        return;
+    }
+
+    if ($info -is [System.IO.FileInfo] -and $info.Extension -match $allowedExtensions) {
+        Compress -file $_;
+        # CopyOriginalFile -File $_ -Destination "$($info.DirectoryName)\Original";
+        return;
+    } 
+
+    $files = Get-ChildItem -LiteralPath $info.FullName -Filter $allowedExtensions;
+    $files | ForEach-Object {
+        Compress -file $_;
+        # CopyOriginalFile -File $_ -Destination "$($info.DirectoryName)\Original";
+    }
+}
+    
