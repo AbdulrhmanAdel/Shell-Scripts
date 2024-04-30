ee4f24e06eb33997f53aad18835f309b4fd174cd
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-04-30 10:50:38 +0300

    Septate Convert PNG to Ico into separate script

---
+++ Icons/Utils/Convert-Png-To-Ico.ps1
@@ -0,0 +1,92 @@
+Add-Type -AssemblyName System.Drawing;
+& Parse-Args.ps1 $args;
+
+function ResizeImage {
+    param (
+        $oldImage,
+        $newWidth,
+        $newHeight
+    )
+    $newImage = New-Object System.Drawing.Bitmap($newWidth, $newHeight);
+    $graphics = [System.Drawing.Graphics]::FromImage($newImage);
+    $graphics.DrawImage($oldImage, 0, 0, $newWidth, $newHeight);
+    $graphics.Dispose();
+    return $newImage;
+}
+
+#Convert The File
+function ConvertImageToIco  (
+    [string] 
+    $sourceImagePath,
+    $newFilePath,
+    $sizes
+) {
+    if (!$sizes) {
+        # $sizes = 256, 128, 64, 48, 32, 16;
+        $sizes = 16, 32, 48, 64, 128, 256;
+    }
+
+    $sourceImage = New-Object System.Drawing.Bitmap($sourceImagePath)
+    $imageStreams = [System.IO.MemoryStream[]]::new($sizes.Length);
+    for ($i = 0; $i -lt $sizes.Length; $i++) {
+        $size = $sizes[$i];
+        $newImage = ResizeImage -oldImage $sourceImage -newWidth $size -newHeight $size;
+        $memStream = New-Object System.IO.MemoryStream;
+        $newImage.Save($memStream, [System.Drawing.Imaging.ImageFormat]::Png);
+        $imageStreams[$i] = $memStream;
+    }
+    $sourceImage.Dispose();
+    if (Test-Path -LiteralPath $newFilePath) {
+        Remove-Item -LiteralPath $newFilePath;
+    }
+
+    $iconFileStream = New-Object System.IO.FileStream($newFilePath, [System.IO.FileMode]::OpenOrCreate);
+    $binaryWriter = New-Object System.IO.BinaryWriter($iconFileStream);
+    
+    $binaryWriter.Write([byte]0);
+    $binaryWriter.Write([byte]0);
+    $binaryWriter.Write([byte]1);
+    $binaryWriter.Write([byte]0); 
+    $binaryWriter.Write([int16] $sizes.Length);
+    
+    $offset = [int](6 + (16 * $sizes.Length));
+    for ($i = 0; $i -lt $imageStreams.Count; $i++) {
+        $size = $sizes[$i];
+        if ($size -gt 255) {
+            $size = 0;
+        } 
+
+        $currentImageStream = $imageStreams[$i];
+        $currentImageLenght = [int]$currentImageStream.Length;
+        $binaryWriter.Write([byte]$size);
+        $binaryWriter.Write([byte]$size);
+        $binaryWriter.Write([byte]0);
+        $binaryWriter.Write([byte]0);
+        $binaryWriter.Write([int16]1);
+        $binaryWriter.Write([int16]32);
+        $binaryWriter.Write($currentImageLenght);
+        $binaryWriter.Write([int]$offset);
+        $offset += $currentImageLenght;
+    }
+    
+    foreach ($imageStream in $imageStreams) {
+        $binaryWriter.Write($imageStream.ToArray());
+    }
+
+    $binaryWriter.Flush();
+    $binaryWriter.Close();
+
+    foreach ($imageStream in $imageStreams) {
+        $imageStream.Dispose();
+    }
+
+    $sourceImage.Dispose();
+}
+
+$pngFilePath ??= $args[0];
+$saveFilePath ??= "$($env:TEMP)\$(Get-Date -Format 'yyyy-MM-dd-HH-mm-ss').ico";
+ConvertImageToIco `
+    -sourceImagePath $pngFilePath `
+    -newFilePath $saveFilePath;
+
+return $saveFilePath;
