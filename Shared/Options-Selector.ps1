0cfbdf833cca4fe4d6fd3132f0e5655f2bb8c1af
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-04-30 08:05:50 +0300

    Improve Options-Selector

--- Shared/Options-Selector.ps1
+++ Shared/Options-Selector.ps1
@@ -4,19 +4,13 @@ Add-Type -AssemblyName System.Windows.Forms
 
 $options = $args[0];
 $title ??= 'Select an Option';
+$mustSelectOne ??= $false;
 
 $form = New-Object System.Windows.Forms.Form
 $form.Text = $title;
 $form.StartPosition = 'CenterScreen'
 $form.Font = New-Object System.Drawing.Font("Segoe UI", 10)
 $form.Width = 500;
-$global:selectedOption = $null
-
-function ButtonClicked {
-    param($senderObj, $e)
-    $global:selectedOption = $senderObj.Tag;
-    $form.DialogResult = [System.Windows.Forms.DialogResult]::OK
-}
 
 $flowLayoutPanel = New-Object System.Windows.Forms.FlowLayoutPanel
 $flowLayoutPanel.Dock = [System.Windows.Forms.DockStyle]::Fill
@@ -24,14 +18,26 @@ $flowLayoutPanel.AutoScroll = $true
 $flowLayoutPanel.Padding = New-Object System.Windows.Forms.Padding(10)  # Add padding
 $form.Controls.Add($flowLayoutPanel)
 
-function CreateButton($text) {
+function CreateButton($option) {
     $button = New-Object System.Windows.Forms.Button
-    $button.Text = $text
-    $button.Tag = $text
+    if ($option.Key) {
+        $button.Text = $option.Key
+        $button.Tag = $option.Value
+    }
+    else {
+        $button.Text = $option
+        $button.Tag = $option
+    }
     $button.AutoSize = $true
     $button.AutoSizeMode = [System.Windows.Forms.AutoSizeMode]::GrowAndShrink
-    $button.Padding = New-Object System.Windows.Forms.Padding(5)  # Add button padding
-    $button.Add_Click({ ButtonClicked $args[0] $args[1] })
+    $button.Padding = New-Object System.Windows.Forms.Padding(5)  # Add button padding 
+    $button.Add_Click(
+        {
+            param ($button)
+            $form.Tag = $button.Tag;
+            $form.DialogResult = [System.Windows.Forms.DialogResult]::OK
+        }
+    )
     $flowLayoutPanel.Controls.Add($button)
 }
 
@@ -44,10 +50,6 @@ while ($mustSelectOne -and $result -ne [System.Windows.Forms.DialogResult]::OK) 
     Write-Host "You Must Select An Option" -ForegroundColor Red;
     $result = $form.ShowDialog();
 }
+
 $form.Dispose();
-if ($result -eq [System.Windows.Forms.DialogResult]::OK -and $global:selectedOption) {
-    return $global:selectedOption;
-}
-else {
-    return $null;
-}
+return $form.Tag;
