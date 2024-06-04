49faa5d5dac861c6f6060cc07ac74de6c64bdced
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-06-04 18:48:59 +0300

    Improve youtube downloader

--- Youtube/Download-Single-video.ps1
+++ Youtube/Download-Single-video.ps1
@@ -1,28 +1,23 @@
 $destinitionPath = $args[0];
 
+$defaultHandler = { & yt-dlp -f "ba" -x --audio-format m4a -o "$destinitionPath\\%(title)s" $url; };
+$options = @{
+    "mp3"    = { & yt-dlp -f "ba" -x --audio-format mp3 --audio-quality 160K  -o "$destinitionPath\\%(title)s" $url; }
+    "m4a"    = { & yt-dlp -f "ba" -x --audio-format m4a --audio-quality 160K  -o "$destinitionPath\\%(title)s" $url; }
+    "480p"   = { & yt-dlp -f "bestvideo[height<=480]+bestaudio[ext=m4a]" -o "$destinitionPath\\%(title)s.%(ext)s" $url; }
+    "720p"   = { & yt-dlp -f "bestvideo[height<=720]+bestaudio[ext=m4a]" -o "$destinitionPath\\%(title)s.%(ext)s" $url; }
+    "Cancel" = { EXIT }
+}
+
 if (!$destinitionPath) {
     $destinitionPath = Read-Host "Please enter destinition path?";
 }
 
-Write-Host "Please enter format you want?"
-Write-Host "1: Audio - MP3";
-Write-Host "2: Audio - M4A";
-Write-Host "3: video 480P";
-Write-Host "4: video 720P";
-$format = Read-Host "Format: ";
-$url = Read-Host "Please enter url?";
-
+$format = & Options-Selector.ps1 -options $options.Keys -title "Please enter format you want?" --mustSelectOne;
 function download {
-    switch ($format) {
-        1 { & yt-dlp -f "ba" -x --audio-format mp3 --audio-quality 160K  -o "$destinitionPath\\%(title)s" $url; }
-        2 { & yt-dlp -f "ba" -x --audio-format m4a --audio-quality 160K  -o "$destinitionPath\\%(title)s" $url; }
-        3 { & yt-dlp -f "bestvideo[height<=480]+bestaudio[ext=m4a]" -o "$destinitionPath\\%(title)s.%(ext)s" $url; }
-        4 { & yt-dlp -f "bestvideo[height<=720]+bestaudio[ext=m4a]" -o "$destinitionPath\\%(title)s.%(ext)s" $url; }
-        Default { & yt-dlp -f "ba" -x --audio-format m4a -o "$destinitionPath\\%(title)s" $url; }
-    }
-
     $url = Read-Host "Please enter url?";
-
+    $handler = $options[$format] ?? $defaultHandler;
+    Invoke-Command -ScriptBlock $handler -NoNewScope;
     if ($url) {
         download;
     }
