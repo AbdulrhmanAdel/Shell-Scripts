ab6f22f4d0601c9926668ff5ec969f9b31ca0a7b
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-09-08 00:33:09 +0300

    Enhance Icon Downloader

--- Icons/Web/Register-Native-Host.ps1
+++ Icons/Web/Register-Native-Host.ps1
@@ -8,11 +8,12 @@ $name = "com.at_dev.icon_downloader"
     description     = "Icon Communicator native messaging host"
     path            = $scriptPath
     type            = "stdio"
-    allowed_origins = @("chrome-extension://$extensionId")
-} | ConvertTo-Json -depth 100 | Out-File "$($PSScriptRoot)\Icon-Communicator.Json";
+    allowed_origins = @("chrome-extension://$extensionId/")
+} | ConvertTo-Json -Depth 100 -Compress | `
+    Out-File "$($PSScriptRoot)\Icon-Communicator.Json";
 
 $key = "HKEY_CURRENT_USER\Software\Google\Chrome\NativeMessagingHosts\$name";
 $value = "$($PSScriptRoot)\Icon-Communicator.Json";
-reg add $key /ve /d $value
+reg add $key /ve /d $value /f;
 & Force-Manually-Exit.ps1;
 
--- Icons/Web/Search-Icon.ps1
+++ Icons/Web/Search-Icon.ps1
@@ -1,7 +1,16 @@
-$args | ForEach-Object {
+$replaceText = "\[(FitGirl|Dodi).*\]";
+$args | Where-Object { Test-Path -LiteralPath $_ } | ForEach-Object {
+    $hasIcon = Get-ChildItem -LiteralPath $_ -Filter "*.ico" -Force;
+    if ($hasIcon) {
+        Write-Host "Folder Has ICON $_" -ForegroundColor Red;
+        return;
+    }
     $info = Get-Item -LiteralPath $_;
-
+    $name = $info.Name -replace $replaceText, "";
+    Write-Host "HANDLING ICON FOR $_" -ForegroundColor Green;
     $url = [System.Web.HttpUtility]::UrlEncode($info.FullName)
     Write-Host $url;
-    Start-Process "https://www.google.com/search?tbm=isch&q=$($info.Name) Game PNG Icon&path=$url";
+    Start-Process "https://www.google.com/search?tbm=isch&q=$($name) Game PNG Icon&path=$url";
 }
+
+timeout.exe 15 /nobreak;
