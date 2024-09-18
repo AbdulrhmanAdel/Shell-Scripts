9e14f03eeae7105c5d4e52678af12b3336687e45
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-09-19 00:48:38 +0300

    Fix Extract-Track

--- Media/Extract-Track.ps1
+++ Media/Extract-Track.ps1
@@ -1,30 +1,22 @@
 #region Functions
 
-$codecExtensionMapper = @{
+$codecSettings = @{
     # Audio
-    "aac"                = ".aac"
-    "m4a"                = ".m4a"
-    "mp3"                = ".mp3"
+    "aac"    = @{ Type = "a"; Encoder = "aac"; Extension = ".aac" }
+    "m4a"    = @{ Type = "a"; Encoder = "aac"; Extension = ".m4a" }
+    "opus"   = @{ Type = "a"; Encoder = "libopus"; Extension = ".opus" }
+    "mp3"    = @{ Type = "a"; Encoder = "libmp3lame"; Extension = ".mp3" }
     # Subtitles
-    "ass"                = ".ass" # ASS (Advanced SSA) subtitle (decoders: ssa ass) (encoders: ssa ass)
-    "srt"                = ".srt" # SubRip subtitle with embedded timing
-    "subrip"             = ".srt" # SubRip subtitle (decoders: srt subrip) (encoders: srt subrip)
-    "dvb_subtitle"       = ".sub" # DVB subtitles (decoders: dvbsub) (encoders: dvbsub)
-    "dvd_subtitle"       = ".sub" # DVD subtitles (decoders: dvdsub) (encoders: dvdsub)
-    "subviewer"          = ".sub" # SubViewer subtitle
-    "subviewer1"         = ".sub" # SubViewer v1 subtitle
-    "hdmv_pgs_subtitle"  = ".sup" # HDMV Presentation Graphic Stream subtitles (decoders: pgssub)
-    "hdmv_text_subtitle" = ".sup" # HDMV Text subtitle
-    "jacosub"            = ".jss" # JACOsub subtitle
-    "microdvd"           = ".sub" # MicroDVD subtitle
-    "mpl2"               = ".mpl" # MPL2 subtitle
-    "pjs"                = ".pjs" # PJS (Phoenix Japanimation Society) subtitle
-    "realtext"           = ".rt" # RealText subtitle
-    "sami"               = ".smi" # SAMI subtitle
-    "ssa"                = ".ssa" # SSA (SubStation Alpha) subtitle
-    "stl"                = ".stl" # Spruce subtitle format
-    "vplayer"            = ".txt" # VPlayer subtitle
-    "webvtt"             = ".vtt" # WebVTT subtitle
+    # ASS (Advanced SSA) subtitle (decoders: ssa ass) (encoders: ssa ass)
+    "ass"    = @{ Type = "s"; Encoder = "ass"; Extension = ".ass" }
+    # SubRip subtitle with embedded timing
+    "srt"    = @{ Type = "s"; Encoder = "srt"; Extension = ".srt" }
+    # SubRip subtitle (decoders: srt subrip) (encoders: srt subrip)
+    "subrip" = @{ Type = "s"; Encoder = "subrip"; Extension = ".srt" }
+    # DVB subtitles (decoders: dvbsub) (encoders: dvbsub)
+    # "dvb_subtitle" = @{ Type = "s"; Encoder = "dvbsub"; Extension = ".sub" }
+    # DVD subtitles (decoders: dvdsub) (encoders: dvdsub)
+    # "dvd_subtitle" = @{ Type = "s"; Encoder = "dvdsub"; Extension = ".idx" }
 };
 
 
@@ -36,38 +28,48 @@ function HandleTrack {
 
     $trackInfo ??= GetTrackInfo($pathInfo.FullName);
     Extract -FileInfo $fileInfo `
-        -index $trackInfo.Index `
-        -extension $trackInfo.Extension;
+        -trackInfo $trackInfo;
 }
 
 function GetTrackInfo($inputPath) {
     $streamsInfo = & ffprobe -v error -print_format json -show_entries `
         "stream=index,codec_name,codec_type,codec_long_name:stream_tags=language" `
         "$inputPath" | ConvertFrom-Json;
-    $tracks = @($streamsInfo.streams | Where-Object { $codecExtensionMapper.ContainsKey($_.codec_name) })
-    foreach ($track in $tracks) {
-        $extension = $codecExtensionMapper[$track.codec_name];
-        Write-Host "$($track.index) => $extension - $($track.codec_long_name) - $($track.codec_type) - $($track.tags.language)";
-    }
-    $streamOrder = (Read-Host "Please Enter StreamOrder") -as [int];
+    $tracks = @($streamsInfo.streams | Where-Object { $codecSettings.ContainsKey($_.codec_name) })
+    $options = $tracks | ForEach-Object {
+        $extension = ($codecSettings[$selectedTrack.codec_name]).Extension;
+        $text = "$($_.tags.language) - $extension - $($_.codec_long_name)";
+        return @{
+            Key   = $text
+            Value = $_.index
+        } 
+    };
+    $streamOrder = Options-Selector.ps1 -options $options --mustSelectOne
     $selectedTrack = $tracks | Where-Object { $_.index -eq $streamOrder };
     return @{
-        Index     = $streamOrder
-        Extension = $codecExtensionMapper[$selectedTrack.codec_name]
+        Index    = $streamOrder
+        Settings = $codecSettings[$selectedTrack.codec_name]
     }
 }
 
 function Extract {
     param (
         [System.IO.FileInfo]$FileInfo,
-        [int]$index,
-        [string]$extension
+        $trackInfo
     )
-
+    $index = $trackInfo.Index;
+    $extension = $trackInfo.Settings.Extension;
+    $type = $trackInfo.Settings.Type;
+    $encoder = $trackInfo.Settings.Encoder;
     $fileDirectoryName = $FileInfo.DirectoryName;
     $fileName = $FileInfo.Name.replace($FileInfo.Extension, "$extension");
     $output = "$fileDirectoryName\$fileName";
-    & ffmpeg "-v" "error" "-i" "$($FileInfo.FullName)" "-map" "0:$index" "$output";
+    Write-Host "ffmpeg '-v' 'error' '-i' '$($FileInfo.FullName)' '-c:$($type)'  $encoder '-map' '0:$index' '$output'"
+    & ffmpeg "-v" "error" `
+        "-i" "$($FileInfo.FullName)" `
+        "-c:$($type)" "copy" `
+        "-map" "0:$index" `
+        "$output";
 }
 
 #endregion
