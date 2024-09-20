f711acb6091a057ed3b770979f9180f561d96c04
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-09-21 02:38:17 +0300

    #TO_RETURN_TO #REMEMBER USE FFMPEG TO CONVERT SUBTITLE INSTEAD OF DOING IT MANUALLY Ass-To-Srt Srt-To-Ass

--- Media/Subtitles/Convertors/Modules/Ass-To-Srt.ps1
+++ Media/Subtitles/Convertors/Modules/Ass-To-Srt.ps1
@@ -1,55 +1,16 @@
-$files = $args[0] | Where-Object { $_ -match "\.ass" };
-
-function ToTimeSpan {
-    param (
-        $time
-    )
-    
-    return [timespan]::ParseExact($time, "h\:mm\:ss\.ff", $null)
-}
-
-function ParseTimeSpan ($timeSpan) {
-    return '{0:00}:{1:00}:{2:00},{3:000}' -f $timeSpan.Hours, $timeSpan.Minutes, $timeSpan.Seconds, $timeSpan.Milliseconds
-}
-function Convert($assFilePath) {
-    $encoding = & Get-File-Encoding.ps1 $assFilePath;
-    Write-Host "USED ENCODING $encoding" -ForegroundColor Green;
-    $content = Get-Content -LiteralPath $assFilePath -Encoding $encoding | ForEach-Object {
-        if ($_ -match "Dialogue: (?<Layer>\d+),(?<StartTime>\d{1,2}:\d{2}:\d{2}\.\d{2}),(?<EndTime>\d{1,2}:\d{2}:\d{2}\.\d{2}),(?<Style>[^,]*),(?<Name>[^,]*),(?<MarginL>\d+),(?<MarginR>\d+),(?<MarginV>\d+),(?<Effect>[^,]*),(?<Text>.+)") {
-            return @{
-                Text      = $Matches["Text"] 
-                StartTime = ToTimeSpan -time $Matches["StartTime"]
-                EndTime   = ToTimeSpan -time $Matches["EndTime"]
-                Layer     = $Matches["Layer"]
-                Style     = $Matches["Style"]
-                Name      = $Matches["Name"]
-                MarginL   = $Matches["MarginL"]
-                MarginR   = $Matches["MarginR"]
-                MarginV   = $Matches["MarginV"]
-                Effect    = $Matches["Effect"]
-            }
-        }
-    } | Sort-Object -Property StartTime;
-    $newContent = New-Object System.Collections.Generic.List[System.Object];
-    for ($i = 0; $i -lt $content.Count; $i++) {
-        $sub = $content[$i];
-        $newContent.Add($i + 1) | Out-Null;
-        $newContent.Add("$(ParseTimeSpan -timeSpan $sub["StartTime"]) --> $(ParseTimeSpan -timeSpan $sub["EndTime"])") | Out-Null;
-        $newContent.Add($sub["Text"]) | Out-Null;
-        $newContent.Add("") | Out-Null;
-    }
-
+$args[0] | Where-Object { $_ -match "\.ass" } | ForEach-Object {
+    $file = $_;
     $srtFileName = $file -replace ".ass", ".srt";
-    $newContent | Set-Content -LiteralPath $srtFileName -Encoding $encoding;
-}
-
-$removeSource = & Prompt.ps1 -title "Remove Source?" -message "Do You Want To Remove Source?" -defaultValue $false;
-foreach ($file in $files) {
-    
-    Convert -assFilePath $file;
-    if ($removeSource) {
-        Remove-Item -LiteralPath $file -Force;
-    }
-}
+    $arguments = @(
+        "-v", "error",
+        "-stats",
+        "-i", """$file"""
+        "-c", "subrip"
+        """$srtFileName"""
+    );
+    Start-Process ffmpeg `
+        -ArgumentList $arguments `
+        -Wait -NoNewWindow -PassThru;
+};
 
 timeout.exe 15;
