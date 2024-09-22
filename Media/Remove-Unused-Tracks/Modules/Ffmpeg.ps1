165d07fa5bfa2508f7ae7e87924bfe589382630c
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-09-22 19:50:58 +0300

    Enhancements

--- Media/Remove-Unused-Tracks/Modules/Ffmpeg.ps1
+++ Media/Remove-Unused-Tracks/Modules/Ffmpeg.ps1
@@ -1,3 +1,28 @@
+$english = @("en", "eng", "english");
+$arabic = @("ara", "ar", "Arabic");
+$notPreferedLanguages = @("hin");
+function GetAudioIds {
+    param (
+        [System.Object[]]$AudioStreams
+    )
+    
+    if ($AudioStreams.Length -eq 1) {
+        return @($AudioStreams[0].index);
+    }
+
+    $accpetedAudio = @($AudioStreams | Where-Object { $_.tags.language -notin $notPreferedLanguages });
+    if ($accpetedAudio.Length -eq 1) {
+        return @($accpetedAudio[0].index);
+    }
+
+    $nonEnglishTracks = @($accpetedAudio | Where-Object { $_.tags.language -notin $english });
+    if ($nonEnglishTracks.Length -ne 0) {
+        return $nonEnglishTracks | ForEach-Object { return $_.index }
+    }
+
+    return @($accpetedAudio[0] ?? $AudioStreams[0]);
+}
+
 $inputPath = $args[0]
 $outputPath = $args[1];
 $streams = (
@@ -13,27 +38,38 @@ $arguments = @(
     "-map", "0:v"
 );
 
-$audioStreams = @($streams | Where-Object { $_.codec_type -eq "audio" });
-$audioStream = $audioStreams | Where-Object { $_.tags.language -notmatch "en|eng|English|hin" } | Select-Object -First 1;
-$audioStream ??= $audioStreams[0];
-$arguments += @(
-    "-map", "0:$($audioStream.index)"
-)
-
-$subtitleStreams = $streams | Where-Object { $_.codec_type -eq "subtitle" };
-$subtitleStreams | Where-Object { $_.tags.language -match "ara|ar|Arabic" } | ForEach-Object {
+#region Audio
+$audioStreamsIds = GetAudioIds -AudioStreams @($streams | Where-Object { $_.codec_type -eq "audio" });
+$audioStreamsIds | ForEach-Object {
     $arguments += @(
-        "-map", "0:$($_.index)",
-        "-disposition:$($_.index)", "default"
+        "-map", "0:$($audio.index)"
+    )
+}
+#endregion
+
+#region Subtitle
+$defaultSub = $null;
+$subtitleStreams = $streams | Where-Object { $_.codec_type -eq "subtitle" };
+$subtitleStreams | Where-Object { $_.tags.language -in $arabic } | ForEach-Object {
+    $defaultSub ??= $_;
+    $arguments += @(
+        "-map", "0:$($_.index)"
     );
 }
-
-$subtitleStreams | Where-Object { $_.tags.language -match "en|eng|English" } | ForEach-Object {
+$subtitleStreams | Where-Object { $_.tags.language -in $english } | ForEach-Object {
+    $defaultSub ??= $_;
     $arguments += @(
         "-map", "0:$($_.index)",
         "-disposition:$($_.index)", "none"
     );
 }
+
+if (!$defaultSub) {
+    $arguments += @(
+        "-disposition:$($defaultSub.index)", "default"
+    );
+}
+#endregion
 
 $arguments += @("-c", "copy");
 $arguments += """$outputPath""";
