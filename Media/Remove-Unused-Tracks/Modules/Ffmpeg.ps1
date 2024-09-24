e41cae0dbaf0014bd46b590c4aad6fa3656d1fcb
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-09-24 12:27:52 +0300

    Fix Remove-Unused-Tracks FFMpeg

--- Media/Remove-Unused-Tracks/Modules/Ffmpeg.ps1
+++ Media/Remove-Unused-Tracks/Modules/Ffmpeg.ps1
@@ -1,3 +1,12 @@
+param (
+    $inputPath,
+    $outputPath
+)
+
+$inputPath ??= $args[0]
+$outputPath ??= $args[1];
+
+
 $english = @("en", "eng", "english");
 $arabic = @("ara", "ar", "Arabic");
 $notPreferedLanguages = @("hin");
@@ -23,8 +32,6 @@ function GetAudioIds {
     return @($accpetedAudio[0] ?? $AudioStreams[0]);
 }
 
-$inputPath = $args[0]
-$outputPath = $args[1];
 $streams = (
     & ffprobe -v error -print_format json -show_entries `
         "stream=index,codec_type:stream_tags=language" `
@@ -35,14 +42,14 @@ $arguments = @(
     "-v", "error",
     "-stats",
     "-i", """$inputPath""", 
-    "-map", "0:v"
+    "-map", "0:0"
 );
 
 #region Audio
 $audioStreamsIds = GetAudioIds -AudioStreams @($streams | Where-Object { $_.codec_type -eq "audio" });
 $audioStreamsIds | ForEach-Object {
     $arguments += @(
-        "-map", "0:$($audio.index)"
+        "-map", "0:$($_)"
     )
 }
 #endregion
@@ -51,22 +58,21 @@ $audioStreamsIds | ForEach-Object {
 $defaultSub = $null;
 $subtitleStreams = $streams | Where-Object { $_.codec_type -eq "subtitle" };
 $subtitleStreams | Where-Object { $_.tags.language -in $arabic } | ForEach-Object {
-    $defaultSub ??= $_;
     $arguments += @(
         "-map", "0:$($_.index)"
     );
+
+    if (!$defaultSub) {
+        $defaultSub = $_;
+        $arguments += @(
+            "-disposition:$($defaultSub.index)", "default"
+        );
+    }
 }
 $subtitleStreams | Where-Object { $_.tags.language -in $english } | ForEach-Object {
-    $defaultSub ??= $_;
     $arguments += @(
         "-map", "0:$($_.index)",
         "-disposition:$($_.index)", "none"
-    );
-}
-
-if (!$defaultSub) {
-    $arguments += @(
-        "-disposition:$($defaultSub.index)", "default"
     );
 }
 #endregion
