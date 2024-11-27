fd15b6d676e208896207940137c804d825fb1f37
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2024-11-28 00:19:14 +0200

    Enhancements and Fixes

--- Shared/Media/Imdb-GetShow.ps1
+++ Shared/Media/Imdb-GetShow.ps1
@@ -6,45 +6,19 @@ param (
     $type
 )
 
-# function Search {
-#     param (
-#         $key
-#     )
-
-#     If (-not (Get-Module -ErrorAction Ignore -ListAvailable PSParseHTML)) {
-#         Write-Verbose "Installing PSParseHTML module for the current user..."
-#         Install-Module -Scope CurrentUser PSParseHTML -ErrorAction Stop
-#     }
-      
-#     $text = ConvertFrom-Html -Engine AngleSharp -Url "https://www.imdb.com/find/?q=$key";
-#     $titles = $text.QuerySelectorAll(".ipc-metadata-list-summary-item");
-#     $titles | ForEach-Object { 
-#         $details = $_.QuerySelector('.ipc-metadata-list-summary-item__tc');
-#         $moviesNameNode = $details.QuerySelector('a')
-#         $moviesId = $moviesNameNode.PathName.Replace("/title/", "").Replace("/", "");
-#         $moviesName = $moviesNameNode.TextContent;
-#         $year = $details.QuerySelector('ul li:nth-child(1)').TextContent; ;
-#         $kind = $details.QuerySelector('ul li:nth-child(2)').TextContent; ;
-        
-#         return @{
-#             Id    = $moviesId
-#             Title = $moviesName
-#             Year  = $year
-#             Kind  = $kind
-#         };
-#     }
-# }
-
-
 function Prompt {
     param (
         $Infos
     )
 
+    if ($Infos.Length -eq 1) {
+        return $Infos[0];
+    }
+
     return Single-Options-Selector.ps1 -Options @(
         $Infos | ForEach-Object {
             return @{
-                Key   = "$($_.id) $($_.l) $($_.y) $($_.qid)"
+                Key   = "$($_.id) - $($_.l) - $($_.y) - $($_.qid)"
                 Value = $_
             }
         }
