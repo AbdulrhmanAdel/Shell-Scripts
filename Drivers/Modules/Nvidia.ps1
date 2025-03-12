c0cd133d7f07b89a32d449691d6c1601a581c895
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2025-03-12 21:51:33 +0200

    Enhancements

--- Drivers/Modules/Nvidia.ps1
+++ Drivers/Modules/Nvidia.ps1
@@ -8,41 +8,82 @@
 #     $OsArchitecture
 # )
 
+$osIdMap = @(
+    @{
+        OsId = 57
+        Name = "Windows 10 64-bit"
+    }
+    @{
+        OsId = 135
+        Name = "Windows 11"
+    }
+    @{
+        OsId = 124
+        Name = "Linux aarch64"
+    }
+    @{
+        OsId = 12
+        Name = "Linux 64-bit"
+    }
+    @{
+        OsId = 22
+        Name = "FreeBSD x64"
+    }
+)
+
+$driverList = @(
+    @{
+        Key      = "NVIDIA GeForce RTX 5090 D"
+        FamilyId = 1
+        DriverId = 1067
+    }
+    @{
+        Key      = "NVIDIA GeForce RTX 5090"
+        FamilyId = 1
+        DriverId = 1066
+    }
+    @{
+        Key      = "NVIDIA GeForce RTX 4070 Super"
+        FamilyId = 127
+        DriverId = 1039
+    }
+)
 
 #region Functions
 function GetHardwareInfo {
-    try {
-        $VideoController = Get-WmiObject -ClassName Win32_VideoController | Where-Object { $_.Name -match "NVIDIA" }
-        $installedVersionDetails = [double]($VideoController.DriverVersion.Replace('.', '')[-5..-1] -join '').insert(3, '.')
-        return @{
-            Version = $installedVersionDetails
-            Name    = $VideoController.Name
-        }
-    }
-    catch {
+    $VideoController = Get-WmiObject -ClassName Win32_VideoController | Where-Object { $_.Name -match "NVIDIA" }
+    if (!$VideoController) {
         return $null;
+    }
+    
+    $installedVersionDetails = [double]($VideoController.DriverVersion.Replace('.', '')[-5..-1] -join '').insert(3, '.')
+    return @{
+        Version = $installedVersionDetails
+        Name    = $VideoController.Name
     }
 }
 
 #endregion
-
-$installedVersionDetails = GetHardwareInfo;
-if (!$installedVersionDetails) {
-    Write-Host -ForegroundColor Yellow "Unable to detect a compatible Nvidia device."
-    Write-Host "Press any key to exit..."
-    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    exit
+$driverInfo = $null;
+$hardwareInfo = GetHardwareInfo;
+if (!$hardwareInfo) {
+    Write-Host -ForegroundColor Red "Unable to detect a compatible Nvidia device."
+    Write-Host -ForegroundColor Yellow "Switching to manual mode, Please select your Nvidia device from the list below:"
+    $driverInfo = Single-Options-Selector.ps1 -Options $driverList;
+}
+else {
+    $driverInfo = $driverList | Where-Object { $_.Key -eq $hardwareInfo.Name } | Select-Object -First 1;
 }
 
 $osID = 135;
-$driverFamilyId = 123
-$driverId = 963;
-
+$driverFamilyId = $driverInfo.FamilyId
+$driverId = $driverInfo.DriverId;
 $res = curl "https://gfwsl.geforce.com/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=$driverFamilyId&pfid=$driverId&osID=$osID&languageCode=1033&beta=null&isWHQL=1&dltype=-1&dch=1&upCRD=null&qnf=0&ctk=null&sort1=1&numberOfResults=1" | ConvertFrom-Json;
-$version = [double]$res.IDS[0].downloadInfo.Version;
-if (!$installedVersionDetails.Version -or $installedVersionDetails.Version -lt $version) {
+$downloadInfo = $res.IDS[0].downloadInfo;
+$version = [double]$downloadInfo.Version;
+if (!$hardwareInfo.Version -or $installedVersionDetails.Version -lt $version) {
     Write-Host "New version available: $version"
 }
 
-$url = $res.IDS[0].downloadInfo.DownloadURL;
+$url = $downloadInfo.DownloadURL;
 Start-Process $url ;
