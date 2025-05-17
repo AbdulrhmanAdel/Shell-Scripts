a16e5cceb0bad09c4240ee56fed225cb8af32401
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2025-05-18 01:46:36 +0300

    Fix SetAsDefault

--- Shared/Set-AsDefaultApp.ps1
+++ Shared/Set-AsDefaultApp.ps1
@@ -17,7 +17,7 @@ $classPath = "HKEY_CURRENT_USER\Software\Classes";
 $Extensions | ForEach-Object {
     $extension = $_;
     $base = $classPath;
-    reg add "$base\$extension" /d $global:programName /f | Out-Null;
+    reg add "$base\$extension" /d "$global:programName$extension" /f | Out-Null;
     $key = "$base\$global:programName$extension\shell\open\command";
     reg add $key /d """$ProgramPath"" ""%1""" /f | Out-Null;
 }
