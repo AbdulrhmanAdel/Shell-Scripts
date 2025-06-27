469c5d8e715571e2ad2e06a1d182d528126d6165
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2025-06-28 01:47:28 +0300

    Add New Script to Check Sync between two folders

---
+++ Tools/Check-FolderSync.ps1
@@ -0,0 +1,69 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory, Position = 0)]
+    [string]
+    $Source,
+    [Parameter(Mandatory, Position = 1)]
+    [string]
+    $Target,
+    [switch]
+    $CheckHashes
+)
+
+$global:checkedFolder = 0;
+$global:checkedFiles = 0;
+function GetTargetFromSource {
+    param (
+        $SourcePath
+    )
+
+    return $SourcePath -replace [regex]::Escape($Source), $Target;
+}
+function CheckFiles {
+    param (
+        [string]$FolderSource
+    )
+
+    $TargetSource = GetTargetFromSource -SourcePath $FolderSource;
+    Get-ChildItem -LiteralPath $FolderSource -File | ForEach-Object {
+        $global:checkedFiles += 1;
+        $filePath = "$TargetSource\$($_.Name)";
+        if (-not (Test-Path -LiteralPath $filePath)) {
+            Write-Host "File not found: $filePath" -ForegroundColor Green;
+        }
+
+        if ($CheckHashes) {
+            $sourceHash = Get-FileHash -LiteralPath $_.FullName -Algorithm SHA256;
+            $targetHash = Get-FileHash -LiteralPath $filePath -Algorithm SHA256;
+            if ($sourceHash.Hash -ne $targetHash.Hash) {
+                Write-Host "File hash mismatch: $filePath" -ForegroundColor Yellow;
+            }
+        }
+    }
+}
+
+
+function CheckFolders {
+    param (
+        [string]$SourceFolder
+    )
+
+    Get-ChildItem -LiteralPath $SourceFolder -Directory | ForEach-Object {
+        $global:checkedFolder += 1; ;
+        $TargetPath = GetTargetFromSource -SourcePath $_.FullName;
+        if (-not (Test-Path -LiteralPath $TargetPath)) {
+            Write-Host "Folder not found: $Target\$path\$($_.Name)" -ForegroundColor Green;
+            return;
+        }
+        else {
+            CheckFolders -SourceFolder $_.FullName;
+        }
+    }
+
+    CheckFiles -FolderSource $SourceFolder;
+}
+
+CheckFolders -SourceFolder $Source;
+
+Write-Host "Checked $global:checkedFolder folders and $global:checkedFiles files.";
+timeout.exe 10;
