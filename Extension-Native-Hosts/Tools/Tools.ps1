2e26ca2f780d4b69deb52eb77bf9dd6513629a65
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2025-06-28 18:32:21 +0300

    Enhance Tools Native Host to use the Youtube-Downloader script from path instead of direct path so I can move it freely without not dependencies V2

--- Extension-Native-Hosts/Tools/Tools.ps1
+++ Extension-Native-Hosts/Tools/Tools.ps1
@@ -7,18 +7,20 @@ Extension-Communicator.ps1 -LogPath "$PSScriptRoot\Logs.txt" -MessageHandler {
         "Save-Torrent" {
             $link = $Data.link;
             $title = $Data.title -replace '[<>:"/\\|?*]', '';
-            $saveLocation = Folder-Picker.ps1 -Retry 1 -ShowOnTop;
-            Create-Shortcut.ps1 -Source $link  -Target "$saveLocation\$title.url";
+            $command = @"
+                `$saveLocation = Folder-Picker.ps1 -Retry 1 -ShowOnTop;
+                Create-Shortcut.ps1 -Source $link -Target "`$saveLocation\$title.url"
+"@
+            Start-Process pwsh.exe -ArgumentList @(
+                "-Command", $command
+            )
             break;
         }
 
         "Download-Video" {
             $link = $Data.link;
-            $scriptPath = Resolve-Path -Path "$PSScriptRoot\..\..\Youtube\Downloader.ps1"
-            Start-Process pwsh.exe  -WindowStyle Maximized -ArgumentList @(
-                "-File", """$scriptPath""",
-                "-Link", $link,
-                "-Format", "opus"
+            Start-Process pwsh.exe -ArgumentList @(
+                "-Command", "Youtube-Downloader.ps1 -Link $link -Format opus"
             )
             break;
         }
