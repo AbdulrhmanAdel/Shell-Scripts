8e565389c959553d0dad669c4d02203acf1bae0d
Abdulrahman Adel <abdulrhmanadel@outlook.com>
2026-06-16 18:21:04 +0300

    feat: Implement link shortening functionality and add SkipDualAudio option in Anidl.ps1

--- Crawlers/Anidl.ps1
+++ Crawlers/Anidl.ps1
@@ -2,8 +2,61 @@
 param (
     [Parameter(Mandatory = $true, Position = 1)]
     [string]
-    $Title
+    $Title,
+    [switch]
+    $SkipDualAudio = $false
 )
+
+function GetLink {
+    param (
+        $Id
+    )
+
+    # {
+    #     "success": true,
+    #     "shortenedUrl": "https://ouo.io/ZfjmrQ",
+    #     "shortener": "ouo.io",
+    #     "cached": false
+    # }
+    $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+    $session.UserAgent = "Mozilla/5.0 (Linux; Android 13; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Mobile Safari/537.36"
+    $Result = Invoke-WebRequest -UseBasicParsing -Uri "https://anidl.org/api/secure-shorten" `
+        -Method "POST" `
+        -WebSession $session `
+        -Headers @{
+        "authority"                   = "anidl.org"
+        "method"                      = "POST"
+        "path"                        = "/api/secure-shorten"
+        "scheme"                      = "https"
+        "accept"                      = "*/*"
+        "accept-encoding"             = "gzip, deflate, br, zstd"
+        "accept-language"             = "en-US,en;q=0.9"
+        "origin"                      = "https://anidl.org"
+        "priority"                    = "u=1, i"
+        "referer"                     = "https://anidl.org/on-going"
+        "sec-ch-ua"                   = "`"Brave`";v=`"147`", `"Not.A/Brand`";v=`"8`", `"Chromium`";v=`"147`""
+        "sec-ch-ua-arch"              = "`"`""
+        "sec-ch-ua-bitness"           = "`"64`""
+        "sec-ch-ua-full-version-list" = "`"Brave`";v=`"147.0.0.0`", `"Not.A/Brand`";v=`"8.0.0.0`", `"Chromium`";v=`"147.0.0.0`""
+        "sec-ch-ua-mobile"            = "?1"
+        "sec-ch-ua-model"             = "`"SM-G981B`""
+        "sec-ch-ua-platform"          = "`"Android`""
+        "sec-ch-ua-platform-version"  = "`"13`""
+        "sec-fetch-dest"              = "empty"
+        "sec-fetch-mode"              = "cors"
+        "sec-fetch-site"              = "same-origin"
+    } `
+        -ContentType "application/json" `
+        -Body "{`"fileId`":`"$Id`"}"
+    
+    $body = $Result.Content | ConvertFrom-Json;
+    if ($body.success) {
+        return $body.shortenedUrl;
+    }
+    else {
+        throw "Failed to shorten link for fileId $Id";
+    }
+}
 
 function GetEpisodeNumber() {
     param (
@@ -34,34 +87,35 @@ if (Test-Path -LiteralPath $Title) {
 Write-Host "Searching for $Title" -ForegroundColor Green;
 $Title = $Title.Trim() -replace "-", " " -replace " +", " " -replace " ", "+";
 $Title = $Title + "+720P";
-
-$Result = curl 'https://anidl.org/wp-admin/admin-ajax.php' `
-    -H 'accept: application/json, text/javascript, */*; q=0.01' `
-    -H 'accept-language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ar;q=0.6,ru;q=0.5,fr;q=0.4,ko;q=0.3' `
-    -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' `
-    -b 'cf_clearance=ZHNcQkClZjDyM874HTroao0J2x2TGJo5P1T8maoYCJY-1749930580-1.2.1.1-yVgEFicPJPWqwdFMWqGkBLBXolw1QQ6hkidPrAUr8JT.wq40HKK1_xUzTu.Cy7pzwNRdKc_xwT9KdtIBc62gmFCR6cuSoHAGiW5mbbMy6dCmMWKxtXR50aQfHULsNkKhjJLeIIfirAatyATj1_gxJR1bwRw6jA5Gn6xGGUrHbs5EakQuJLcv1yLp9RLjBqH4GSfKV6RZsajQNc.jwrLQ9_9LtmDtyr6MD4hKUaUMovKbvO6kzMi6IV26e9dbf.G7H654iBndBCJ1xnlAqYRJHF4I8pvaScmNS_oYVK2cepXiaXrdqf8uVr4Lj31quisv4nTpPdeu2D9M9t3Salfa5XhowQCPivbvnIZZCfr1Hyw' `
-    -H 'dnt: 1' `
-    -H 'origin: https://anidl.org' `
-    -H 'priority: u=1, i' `
-    -H 'referer: https://anidl.org/airing-anime/' `
-    -H 'sec-ch-ua: "Microsoft Edge";v="137", "Chromium";v="137", "Not/A)Brand";v="24"' `
-    -H 'sec-ch-ua-arch: "x86"' `
-    -H 'sec-ch-ua-bitness: "64"' `
-    -H 'sec-ch-ua-full-version: "137.0.3296.83"' `
-    -H 'sec-ch-ua-full-version-list: "Microsoft Edge";v="137.0.3296.83", "Chromium";v="137.0.7151.104", "Not/A)Brand";v="24.0.0.0"' `
-    -H 'sec-ch-ua-mobile: ?0' `
-    -H 'sec-ch-ua-model: ""' `
-    -H 'sec-ch-ua-platform: "Windows"' `
-    -H 'sec-ch-ua-platform-version: "19.0.0"' `
-    -H 'sec-fetch-dest: empty' `
-    -H 'sec-fetch-mode: cors' `
-    -H 'sec-fetch-site: same-origin' `
-    -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0' `
-    -H 'x-requested-with: XMLHttpRequest' `
-    --data-raw "draw=4&columns%5B0%5D%5Bdata%5D=tv_icon&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=filename&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=code&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=date&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=100&search%5Bvalue%5D=$Title&search%5Bregex%5D=false&action=get_mongodb_data"
-    
-$responseBody = $Result | ConvertFrom-Json;
-$data = $responseBody.data;
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$session.UserAgent = "Mozilla/5.0 (Linux; Android 13; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Mobile Safari/537.36"
+# $session.Cookies.Add((New-Object System.Net.Cookie("cf_clearance", "DFe2oqmI0m2enLsgz_qH_lTm.FvFqSvLCUO.HqMV4TY-1781620990-1.2.1.1-fZqqX55VmIsAngEWNVeYFFE1JSlljWPNtRHoaFGD5mw0O_SYp9zSH2sFmsepEC1fgLMEg4bLoOvm5noYqL7Knpam6kbeqKUuNM04R.WF9tKxg88GzObCNrFJ_sWAXHyByQKjOEKZ0Sz2ySh1AIpttze7lJQ07H45bRetVkRUoAOPngyIbt7dSr4G_38jMuFW13m0vOCTUnbAw2uSv35OT6oxiCES0NEI.2jO23tTCd3zQuxES1jpfogAAQs1TZNlp7WjqvEHCEk0q_ShYMZkb915grS_3.r9xc5_UhUABUYGJ_Opxz92n.tf4svYDP2QAH8UfkeD9I3qP4ts4s1MwQ", "/", ".anidl.org")))
+$Result = Invoke-WebRequest -UseBasicParsing -Uri "https://anidl.org/api/on_air_data?page=1&limit=25&search=$Title&sortField=date&sortOrder=desc" `
+    -WebSession $session `
+    -Headers @{
+    "authority"                   = "anidl.org"
+    "method"                      = "GET"
+    "path"                        = "/api/on_air_data?page=1&limit=25&search=$Title&sortField=date&sortOrder=desc"
+    "scheme"                      = "https"
+    "accept"                      = "*/*"
+    "accept-encoding"             = "gzip, deflate, br, zstd"
+    "accept-language"             = "en-US,en;q=0.9"
+    "priority"                    = "u=1, i"
+    "referer"                     = "https://anidl.org/on-going"
+    "sec-ch-ua"                   = "`"Brave`";v=`"147`", `"Not.A/Brand`";v=`"8`", `"Chromium`";v=`"147`""
+    "sec-ch-ua-arch"              = "`"`""
+    "sec-ch-ua-bitness"           = "`"64`""
+    "sec-ch-ua-full-version-list" = "`"Brave`";v=`"147.0.0.0`", `"Not.A/Brand`";v=`"8.0.0.0`", `"Chromium`";v=`"147.0.0.0`""
+    "sec-ch-ua-mobile"            = "?1"
+    "sec-ch-ua-model"             = "`"SM-G981B`""
+    "sec-ch-ua-platform"          = "`"Android`""
+    "sec-ch-ua-platform-version"  = "`"13`""
+    "sec-fetch-dest"              = "empty"
+    "sec-fetch-mode"              = "cors"
+    "sec-fetch-site"              = "same-origin"
+};
+$responseBody = $Result.Content | ConvertFrom-Json;
+$data = $responseBody.files;
 if ($data.Length -eq 0) {
     Write-Host "No results found" -ForegroundColor Red;
     timeout.exe 5;
@@ -69,19 +123,17 @@ if ($data.Length -eq 0) {
 
 $links = $data  | Where-Object { $_.filename -match "720p" } | ForEach-Object {
     $fileName = $_.filename;
-    $fileName -match "(?<Size>\d+ MBs)" | Out-Null;
-    $hasArabicSub = $fileName.Contains("fi fi-sa");
-    $size = $matches["Size"];
-    $endIndex = $fileName.IndexOf(" <span");
-    $fileName = $fileName.Substring(4, $endIndex - 4);
-    $link = $_.code;
-    $linkEndIndex = $link.IndexOf(""" ");
-    $link = $link.Substring(9, $linkEndIndex - 9);
- 
+    $arabicSubsCount = $_.subtitle | `
+        Where-Object { $_.lang -match "Arabic|ARA" -or $_.flag -match "fi-sa" } | `
+        Measure-Object | Select-Object -ExpandProperty Count
+
     return @{
-        Key      = "$fileName $size $hasArabicSub";
-        FileName = $fileName
-        Value    = $link -replace "#038;", "";
+        Source       = $_
+        Id           = $_.id
+        FileName     = $fileName
+        Audio        = $_.audio
+        Size         = $_.size
+        HasArabicSub = $arabicSubsCount -ge 1
     }
 };
 
@@ -98,14 +150,43 @@ if ($links.Length -eq 0) {
     Exit;
 }
 
+
+$links = $links | Where-Object {
+    if (!$SkipDualAudio) {
+        return $true;
+    }
+    
+    return $_.Audio.Count -eq 1
+} | ForEach-Object {
+    return @{
+        Key   = $_.FileName;
+        Value = $_
+    }
+}
+
 $DownloadLinks = Multi-Options-Selector.ps1 -options $links;
 if ($DownloadLinks.Count -eq 0) {
     return;
 }
 
 $DownloadLinks | ForEach-Object {
-    Start-Process $_;
+    $link = $null;
+    while (-not $link) {
+        $temp = GetLink -Id $_.Id;
+        if ($temp -match "https://ouo.io/") {
+            $link = $temp;
+        }
+        else {
+            Write-Host "Generated shorten is not supported retring..." -ForegroundColor Yellow;
+            timeout.exe 2;
+        }
+    }
+    $_.Link = $link;
+    Start-Process $link;
 }
 
+$DownloadLinks = $DownloadLinks | ForEach-Object {
+    return $_.Link;
+};
 Set-Clipboard -Value ($DownloadLinks -join "`n")
 timeout.exe 5;
